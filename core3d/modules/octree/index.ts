4d4e964d689dd7fd2eb9ab3dfd7f37390382f6c6
Tore Lode <16665785+torelode@users.noreply.github.com>
2023-03-29 13:30:10 +0800

    ...

--- core3d/modules/octree/index.ts
+++ core3d/modules/octree/index.ts
@@ -28,7 +28,7 @@ export class OctreeModule implements RenderModule {
     } as const satisfies Record<string, UniformTypes>;
 
     readonly gradientImageParams: TextureParams2DUncompressed = { kind: "TEXTURE_2D", width: Gradient.size, height: 2, internalFormat: "RGBA8", type: "UNSIGNED_BYTE", image: null };
-    readonly nodeLoaderOptions: NodeLoaderOptions = { useWorker: false }; // set to false for better debugging
+    readonly nodeLoaderOptions: NodeLoaderOptions = { useWorker: true }; // set to false for better debugging
     readonly maxHighlights = 8;
 
     async withContext(context: RenderContext) {
