44b754bcddad31f5b022b5c7de4b6f89946e1e1d
Tore Lode <16665785+torelode@users.noreply.github.com>
2023-07-24 14:19:15 +0800

    ...

--- web_app/index.ts
+++ web_app/index.ts
@@ -6,6 +6,6 @@ export * from "./controller";
 export * from "./serviceWorker";
 export type { DeviceProfile } from "core3d/device";
 export { getDeviceProfile };
-export const packageVersion = "env" in import.meta ? import.meta.env.NPM_PACKAGE_VERSION : undefined ?? "beta";
+export const packageVersion = "env" in import.meta ? (import.meta as any).env.NPM_PACKAGE_VERSION : undefined ?? "beta";
 
 glMatrix.setMatrixArrayType(Array);
