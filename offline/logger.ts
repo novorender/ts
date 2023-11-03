6a0f70ed3c7e405570f4dfc69643cf94191104cd
Sigve Bergslien <sigvebergslien@hotmail.com>
2023-11-03 10:30:40 +0100

    Offline, remove undefined progress value

--- offline/logger.ts
+++ offline/logger.ts
@@ -25,10 +25,10 @@ export interface Logger {
 
     /**
      * Update progress for potentially long running processes, such as synchronization.
-     * @param value The progress as a factor between 0 and {@link max}, or undefined to hide progress bar.
+     * @param value The progress as a factor between 0 and {@link max}.
      * @param max The maximum value, or `undefined` if value is also undefined.
      * @param operation The operation currently being progressed.
      */
-    progress?(value: number | undefined, max: number | undefined, operation: "scan" | "download"): void;
+    progress?(value: number, max: number | undefined, operation: "scan" | "download"): void;
 }
 
